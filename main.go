687b789fc7989c748a48b1e7d72f6ac85b3ef66e
TingyuSleep <525605299@qq.com>
2024-02-25 20:12:39 +0800

    集成 gin

--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello go!")
+	//new一个Gin Engine实例
+	r := gin.New()
+
+	//注册路由中间件
+	r.Use(gin.Logger(), gin.Recovery())
+
+	//注册路由
+	r.GET("/", func(c *gin.Context) {
+		//以json格式响应
+		c.JSON(http.StatusOK, gin.H{
+			"Hello": "World!",
+		})
+	})
+
+	// 处理 404 请求
+	r.NoRoute(func(c *gin.Context) {
+		// 获取标头信息的 Accept 信息
+		acceptString := c.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			// 如果是 HTML 的话
+			c.String(http.StatusNotFound, "页面返回 404")
+		} else {
+			// 默认返回 JSON
+			c.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
+			})
+		}
+	})
+
+	//运行服务,默认为8080，指定为8000
+	r.Run(":8000")
 }
